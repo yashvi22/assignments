17c5d517bac20d4404e072c3d104e446103b4147
unknown <yashvihp22@gmail.com>
2021-01-12 19:44:34 +0530

    final commit

---
+++ Web Api Final Assignment/HotelManagement/HotelManagement/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HotelManagement.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult GetHotel()
+        {
+            return Ok();
+        }
+    }
+}
