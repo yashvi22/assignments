d1aa35ee6ba690b112d80699f4faf8792a9b32d0
unknown <yashvihp22@gmail.com>
2021-01-14 20:17:34 +0530

    final commit

--- Web Api Final Assignment/HotelManagement/HotelManagement/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HotelManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Web Api Final Assignment/HotelManagement/HotelManagement/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HotelManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
